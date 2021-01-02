077b26e64e0e7eef8f1066d03445da873846a818
GreedoRen <greedo.it@gmail.com>
2021-01-02 19:32:58 +0300

    v-if v-else

--- src/components/userCard.vue
+++ src/components/userCard.vue
@@ -1,33 +1,15 @@
 <template>
   <div class="root">
-    <h1 class="h1">Заголовок</h1>
-    <div class="info">
-      <h2 class="h2">{{ getAuthorsFullName }}</h2>
-
-      <ul>
-        <li>#1</li>
-        <li>#2</li>
-        <li>#3</li>
-      </ul>
+    <div class="flex">
+      <div class="img wrapper">
+        <img v-if="isCatVisible" src="../assets/logo.png" alt="logo" />
+        <p v-else>Скрыто</p>
+      </div>
+      <div class="controls">
+        <h1>Заголовок</h1>
+        <button @click="isCatVisible = !isCatVisible">Показать/скрыть</button>
+      </div>
     </div>
-    <span>Кол-во: {{ users.length }}</span>
-    <ul>
-      <li v-for="(value, key, index) in users[0]" :key="index">
-        {{ value }}
-      </li>
-    </ul>
-    <button type="button" @click="currentPage--">-</button>
-    <button
-      v-for="page in pages"
-      :key="page"
-      @click="currentPage = page"
-      type="button"
-    >
-      {{ page }}
-    </button>
-    <button type="button" @click="currentPage++">+</button>
-
-    <p>Страница {{ currentPage }} из {{ pages }}</p>
   </div>
 </template>
 
@@ -36,38 +18,12 @@ export default {
   name: "userCard",
   data() {
     return {
-      firstName: "Ivan",
-      secondName: "Ivanovich",
-      lastName: "NotIvanov",
-
-      users: [
-        { id: "1", name: "Jhon" },
-        { id: "2", name: "Marie" },
-        { id: "3", name: "Roger" },
-      ],
-      pages: 3,
-      currentPage: 1,
+      isCatVisible: true,
     };
   },
-  computed: {
-    getAuthorsFullName() {
-      return `${this.firstName} ${this.secondName} ${this.lastName}`.toUpperCase();
-    },
-  },
-  methods: {
-    getIdAndName(user) {
-      return `ID: ${user.id};
-            Name: ${user.name};`;
-    },
-    loadUser(page) {
-      console.log(page);
-    },
-  },
-  watch: {
-    currentPage(page) {
-      this.loadUser(page);
-    },
-  },
+  computed: {},
+  methods: {},
+  watch: {},
 };
 </script>
 
