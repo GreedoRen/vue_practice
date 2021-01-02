f6ff0d6b57109062e487a4c4fe09b4c630206f40
GreedoRen <greedo.it@gmail.com>
2021-01-02 20:04:24 +0300

    computed style

--- src/components/userCard.vue
+++ src/components/userCard.vue
@@ -7,6 +7,7 @@
           class="logo"
           :class="imgFilters"
           src="../assets/logo.png"
+          :style="imgStyles"
           alt="logo"
         />
         <!-- <p v-else>Скрыто</p> -->
@@ -14,12 +15,59 @@
       <div class="controls">
         <h1>Заголовок</h1>
         <div class="buttons">
-          <button @click="isCatVisible = !isCatVisible" type="button">
+          <button
+            @click="isCatVisible = !isCatVisible"
+            :class="isCatVisible ? '' : 'active'"
+            type="button"
+          >
             Показать/скрыть
           </button>
-          <button type="button">Уменьшить</button>
-          <button type="button">Фильтр</button>
-          <button type="button">Рамка</button>
+          <button
+            type="button"
+            :class="imgFilters.minimize ? 'active' : ''"
+            @click="imgFilters.minimize = !imgFilters.minimize"
+          >
+            Уменьшить
+          </button>
+          <button
+            type="button"
+            :class="imgFilters.sepia ? 'active' : ''"
+            @click="imgFilters.sepia = !imgFilters.sepia"
+          >
+            Фильтр
+          </button>
+          <button
+            type="button"
+            :class="imgFilters.border ? 'active' : ''"
+            @click="imgFilters.border = !imgFilters.border"
+          >
+            Рамка
+          </button>
+        </div>
+        <div class="sizes">
+          {{ imgSizes.currentWidth }}
+          <label for="width"
+            ><input
+              type="range"
+              name="width"
+              id="width"
+              :value="imgSizes.currentWidth"
+              @input="imgSizes.currentWidth = $event.target.value"
+              :min="imgSizes.minWidth"
+              :max="imgSizes.maxWidth"
+          /></label>
+
+          {{ imgSizes.currentHeight }}
+          <label for="height"
+            ><input
+              type="range"
+              name="height"
+              id="height"
+              :value="imgSizes.currentHeight"
+              @input="imgSizes.currentHeight = $event.target.value"
+              :min="imgSizes.minHeight"
+              :max="imgSizes.maxHeight"
+          /></label>
         </div>
       </div>
     </div>
@@ -37,29 +85,48 @@ export default {
         border: false,
         minimize: false,
       },
+      imgSizes: {
+        maxWidth: "500",
+        maxHeight: "500",
+        minWidth: "100",
+        minHeight: "100",
+        currentWidth: "500",
+        currentHeight: "500",
+      },
     };
   },
-  computed: {},
+  computed: {
+    imgStyles() {
+      return {
+        width: `${this.imgSizes.currentWidth}px`,
+        height: `${this.imgSizes.currentHeight}px`,
+      };
+    },
+  },
   methods: {},
   watch: {},
 };
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 .logo {
   width: 300px;
   height: 300px;
+}
 
-  &.sepia {
-    filter: sepia(100%);
-  }
+.logo.sepia {
+  filter: sepia(100%);
+}
 
-  &.border {
-    border: 2 px solid red;
-  }
+.logo.border {
+  border: 2px solid red;
+}
 
-  &.minimize {
-    transform: scale(0.5);
-  }
+.logo.minimize {
+  transform: scale(0.5);
+}
+
+button.active {
+  background-color: #c2c2c2;
 }
 </style>
