35de9a28ef10378f2b079b50b0108c29b970e242
GreedoRen <greedo.it@gmail.com>
2021-01-02 16:59:18 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <MyComponent />
+  </div>
+</template>
+
+<script>
+import MyComponent from "./components/MyComponent";
+
+export default {
+  name: "App",
+  components: {
+    MyComponent,
+  },
+};
+</script>
+
+<style>
+#app {
+  background-color: black;
+  color: white;
+}
+</style>
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="root">
+    <label>
+      Введите email
+
+      <input type="email" v-bind:value="email" />
+    </label>
+    <label>
+      Введите password
+
+      <input type="password" v-bind:value="password" />
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyComponent",
+  data() {
+    return {
+      email: "test@email.com",
+      password: "testPassword",
+    };
+  },
+};
+</script>
