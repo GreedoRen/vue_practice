a48bae517a94aeb4b9f587d73f3296b41dd3a14d
GreedoRen <greedo.it@gmail.com>
2021-01-03 10:17:47 +0300

    filter v-for

--- src/components/userCard.vue
+++ src/components/userCard.vue
@@ -1,76 +1,11 @@
 <template>
   <div class="root">
-    <div class="flex">
-      <div class="img wrapper">
-        <img
-          v-show="isCatVisible"
-          class="logo"
-          :class="imgFilters"
-          src="../assets/logo.png"
-          :style="imgStyles"
-          alt="logo"
-        />
-        <!-- <p v-else>Скрыто</p> -->
-      </div>
-      <div class="controls">
-        <h1>Заголовок</h1>
-        <div class="buttons">
-          <button
-            @click="isCatVisible = !isCatVisible"
-            :class="isCatVisible ? '' : 'active'"
-            type="button"
-          >
-            Показать/скрыть
-          </button>
-          <button
-            type="button"
-            :class="imgFilters.minimize ? 'active' : ''"
-            @click="imgFilters.minimize = !imgFilters.minimize"
-          >
-            Уменьшить
-          </button>
-          <button
-            type="button"
-            :class="imgFilters.sepia ? 'active' : ''"
-            @click="imgFilters.sepia = !imgFilters.sepia"
-          >
-            Фильтр
-          </button>
-          <button
-            type="button"
-            :class="imgFilters.border ? 'active' : ''"
-            @click="imgFilters.border = !imgFilters.border"
-          >
-            Рамка
-          </button>
-        </div>
-        <div class="sizes">
-          {{ imgSizes.currentWidth }}
-          <label for="width"
-            ><input
-              type="range"
-              name="width"
-              id="width"
-              :value="imgSizes.currentWidth"
-              @input="imgSizes.currentWidth = $event.target.value"
-              :min="imgSizes.minWidth"
-              :max="imgSizes.maxWidth"
-          /></label>
-
-          {{ imgSizes.currentHeight }}
-          <label for="height"
-            ><input
-              type="range"
-              name="height"
-              id="height"
-              :value="imgSizes.currentHeight"
-              @input="imgSizes.currentHeight = $event.target.value"
-              :min="imgSizes.minHeight"
-              :max="imgSizes.maxHeight"
-          /></label>
-        </div>
-      </div>
-    </div>
+    <input type="text" @input="text = $event.target.value" />
+    <ul>
+      <li v-for="(name, index) in searchNames" :key="index">
+        {{ name }}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -79,28 +14,15 @@ export default {
   name: "userCard",
   data() {
     return {
-      isCatVisible: true,
-      imgFilters: {
-        sepia: false,
-        border: false,
-        minimize: false,
-      },
-      imgSizes: {
-        maxWidth: "500",
-        maxHeight: "500",
-        minWidth: "100",
-        minHeight: "100",
-        currentWidth: "500",
-        currentHeight: "500",
-      },
+      text: "",
+      users: ["Jack", "Jhon", "Mike"],
     };
   },
   computed: {
-    imgStyles() {
-      return {
-        width: `${this.imgSizes.currentWidth}px`,
-        height: `${this.imgSizes.currentHeight}px`,
-      };
+    searchNames() {
+      return this.users.filter((name) =>
+        name.toLowerCase().includes(this.text.toLowerCase())
+      );
     },
   },
   methods: {},
@@ -109,24 +31,4 @@ export default {
 </script>
 
 <style scoped>
-.logo {
-  width: 300px;
-  height: 300px;
-}
-
-.logo.sepia {
-  filter: sepia(100%);
-}
-
-.logo.border {
-  border: 2px solid red;
-}
-
-.logo.minimize {
-  transform: scale(0.5);
-}
-
-button.active {
-  background-color: #c2c2c2;
-}
 </style>
