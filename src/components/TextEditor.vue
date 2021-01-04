9dd02ce5ef837d9d5cf5e86aaf18ce2024669c0d
GreedoRen <greedo.it@gmail.com>
2021-01-04 13:19:07 +0300

    TextEditor

---
+++ src/components/TextEditor.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="text-editor">
+    <textarea v-model="text"></textarea>
+    <div class="output-text">{{ text }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: "",
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+textarea {
+  width: 500px;
+  height: 200px;
+}
+</style>
