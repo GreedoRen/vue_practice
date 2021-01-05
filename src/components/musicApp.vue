e2b4b5a039086ca1272115ea41191c25ecf0282d
GreedoRen <greedo.it@gmail.com>
2021-01-05 19:16:36 +0300

    init

---
+++ src/components/musicApp.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <header>
+      <h1>Music</h1>
+    </header>
+    <main>
+      <section class="player">
+        <h2 class="song-title">{{ current.title }}</h2>
+      </section>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "music-app",
+  data() {
+    return {
+      current: {
+        title: "",
+      },
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style scoped>
+</style>
